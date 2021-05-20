354f50a1d36436f463ae6dadfaf1c7ee33b43861
juliendousset <77361261+juliendousset@users.noreply.github.com>
2021-05-19 21:07:28 -0600

    final-website Update

--- final-website/js/main.js
+++ final-website/js/main.js
@@ -15,3 +15,48 @@ function showDropDown() {
 function hideDropDown() {
     document.getElementById('sidebar').style.display = 'none';
 }
+
+// Functions for Home Menu
+function showHomeMenu() {
+    document.getElementById('homeadded').style.display = 'block';
+}
+
+function hideHomeMenu() {
+    document.getElementById('homeadded').style.display = 'none';
+}
+
+function normalBackgroundColor() {
+    document.body.style.backgroundColor = "#f2f2f2";
+    document.getElementById('mainbody').style.backgroundColor = 'white';
+    document.getElementById('mainbody').style.boxShadow = '0 0 18px #ccc'
+}
+
+function changeBackgroundBlack() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById('mainbody').style.backgroundColor = 'black';
+    document.getElementById('mainbody').style.boxShadow = '0 0 0px #ccc'
+}
+
+function changeBackgroundGray() {
+    document.body.style.backgroundColor = "gray";
+    document.getElementById('mainbody').style.backgroundColor = 'gray';
+    document.getElementById('mainbody').style.boxShadow = '0 0 0px #ccc'
+}
+
+function changeBackgroundOrange() {
+    document.body.style.backgroundColor = "orange";
+    document.getElementById('mainbody').style.backgroundColor = 'orange';
+    document.getElementById('mainbody').style.boxShadow = '0 0 0px #ccc'
+}
+
+function changeBackgroundCyan() {
+    document.body.style.backgroundColor = "cyan";
+    document.getElementById('mainbody').style.backgroundColor = 'cyan';
+    document.getElementById('mainbody').style.boxShadow = '0 0 0px #ccc'
+}
+
+function changeBackgroundGreen() {
+    document.body.style.backgroundColor = "lightgreen";
+    document.getElementById('mainbody').style.backgroundColor = 'lightgreen';
+    document.getElementById('mainbody').style.boxShadow = '0 0 0px #ccc'
+}
